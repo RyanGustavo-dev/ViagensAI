6672b79312bd513de17dd71877e256b7df6cf0b4
Ryan Gustavo Torres <ryryan009@gmail.com>
2026-06-25 18:28:04 +0000

    feat: add new migrate

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+from app.extension import db, migrate
+from app.config.settings import config_map
+
+def create_app():
+    app = Flask(__name__)
+
+    env = os.getenv("FLASK_ENV", "development")
+    app.config.from_object(config_map[env])
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    #REGISTRAR BLUEPRINTS
+
+    return app

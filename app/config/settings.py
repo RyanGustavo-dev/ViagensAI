db290cfecdfc8fc36828e93e1e51520c901cfdc8
Ryan Gustavo Torres <ryryan009@gmail.com>
2026-06-25 14:43:06 +0000

    feat: add intial configs

---
+++ app/config/settings.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY","fallback-inseguero")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+config_map = {
+    "development": DevelopmentConfig
+}
